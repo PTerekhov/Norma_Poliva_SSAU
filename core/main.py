2cb243b98aecfafc52798745de4235e0582dc09a
Denis Stepanov <denis.stepanov@macys.com>
2020-09-28 18:33:11 +0400

    scanning files in data_base diretory

---
+++ core/main.py
@@ -0,0 +1,18 @@
+import os
+from os import walk
+
+
+def main():
+    data_files_list = []
+    root_dir = os.getcwd()
+    file_path = root_dir + '/data_base/'
+
+    ### получаем список файлов в директории data_base
+    for (dirpath, dirnames, filenames) in walk(file_path):
+        data_files_list.extend(filenames)
+        break
+    print(data_files_list)
+
+
+if __name__ == '__main__':
+    main()
