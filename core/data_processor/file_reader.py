d9ee43616897d4bb0588c7fd228a8b51570e6e01
pterekhov <terexoff.paw@yandex.ru>
2020-10-08 09:26:23 +0400

    reading xlsx files added

---
+++ core/data_processor/file_reader.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+class file_reader:
+    def __init__(self, file_path):
+        self.data_files_list = []
+        self.data_frames_dict = {}
+        self.FILE_PATH = file_path
+        for (dirpath, dirnames, filenames) in os.walk(self.FILE_PATH):
+            self.data_files_list.extend(filenames)
+            break
+
+    def read_files(self):
+        for filename in self.data_files_list:
+            file = self.FILE_PATH + filename
+            self.data_frames_dict.update({filename: pd.read_excel(file)})
--- core/main.py
+++ core/main.py
@@ -1,20 +1,25 @@
 import os
-from os import walk
+import core.data_processor.file_reader as FR
 
 
 def main():
-    data_files_list = []
-    root_dir = os.getcwd()
-    file_path = root_dir + '/data_base/'
+    file_path = os.getcwd() + '/data_base/'
+    worker = FR.file_reader(file_path)
+    worker.read_files()
+    data_frame = worker.data_frames_dict
+    choice = None
 
-    ### получаем список файлов в директории data_base
-    for (dirpath, dirnames, filenames) in walk(file_path):
-        data_files_list.extend(filenames)
-        break
-    print(data_files_list)
+    stages_and_frames = [['тип почвы', 'type_soil.xlsx', data_frame.get('type_soil.xlsx')],
+                         ['подтип почвы', 'type_gran.xlsx', data_frame.get('type_gran.xlsx')],
+                         ['сельскохозяйственная культура', 'spr_cul.xlsx', data_frame.get('spr_cul.xlsx')],
+                         ['фаза роста', 'dan_cul.xlsx', data_frame.get('dan_cul.xlsx')]]
 
+    variables_list = []
+    # .name_poch
+    # .name_mehs
+    # .name_kul
+    # .name_kul
 
-    
 
 
 if __name__ == '__main__':
